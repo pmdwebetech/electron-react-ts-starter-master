0fe69ffbe3e2fe4d969d1cc5528a1ea616390662
superstardjgame <paddy@superstardjgame.com>
2022-01-18 21:35:10 +0000

    kbbwkdjc

--- public/electron.js
+++ public/electron.js
@@ -5,6 +5,10 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 const { ipcMain } = require('electron'); 
 const { PRINT_LABEL_NEEDED } = require('../src/actions/types')
+const nodeHtmlToImage = require('node-html-to-image')
+const fs = require('fs');
+const { dialog } = require('electron')
+
 
 
 let mainWindow;
@@ -37,6 +41,56 @@ app.on("activate", () => {
     }
 });
 
-ipcMain.on(PRINT_LABEL_NEEDED, (event , data) => {
+ipcMain.on(PRINT_LABEL_NEEDED, async (event , data) => {
     console.log("PRINT_LABEL_NEEDED");
-})
+
+    if (!fs.existsSync(__dirname + '/tmp')) {
+        fs.mkdir(__dirname + '/tmp', function (err) {
+        if (err) {
+            dialog.showErrorBox('Failed to create directory\n', err.message);
+        } else {
+            nodeHtmlToImage({
+                output: __dirname + '/tmp/image.png',
+                html: '<html><body>Hello world!</body></html>'
+              })
+            .then((a, b) => console.log('The image was created successfully!'));
+        }
+        });
+    }
+    else{
+        const x = await nodeHtmlToImage({
+            //output: __dirname + '/tmp/image.png',
+            html: '<html><body>Hello world!</body></html>'
+          });
+        // .then((a, b) => console.log('The image was created successfully!'));
+
+        print(x);
+
+        var d = "";
+    }
+
+    
+});
+
+function print(text){
+    let win = new BrowserWindow({show: false})
+    fs.writeFile(path.join(__dirname,'print.png'), text, function (err) {
+        if(err){
+            dialog.showErrorBox('Failed to print\n', err.message);
+        }
+        else{
+            win.loadURL('file://'+__dirname+'/print.png')
+            win.webContents.on('did-finish-load', () => {
+                win.webContents.print({silent:true}, function (success,failure) {
+                    if(!success){
+                        dialog.showErrorBox('Failed to print\n', failure);
+                    }
+                })
+                // setTimeout(function(){
+                //     win.close();
+                // }, 1000);
+            })
+        }
+    })
+    
+  }
